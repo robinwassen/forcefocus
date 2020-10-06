894be956eb7b2785f87f845aa8ed6d468e283694
Robin Andersson <robin.andersson@digiexam.se>
2020-10-06 21:58:30 +0200

    Fixed fetching the environment variable for NodeJS on AppVeyor

--- scripts/prebuild-publish.bat
+++ scripts/prebuild-publish.bat
@@ -2,7 +2,7 @@
 
 if %APPVEYOR_REPO_BRANCH% == master (
   if %GITHUB_TOKEN% neq "" (
-    npm run prebuild-release -- -u %GITHUB_TOKEN% -t %TRAVIS_NODE_VERSION% -r node
-    npm run prebuild-release -- -u %GITHUB_TOKEN% -t %TRAVIS_NODE_VERSION% -r electron
+    npm run prebuild-release -- -u %GITHUB_TOKEN% -t %1 -r node
+    npm run prebuild-release -- -u %GITHUB_TOKEN% -t %1 -r electron
   )
 )
