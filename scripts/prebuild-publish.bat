ca22053b8a915ce5a90975062d9ec4a8f02d25b1
Robin Andersson <robin.andersson@digiexam.se>
2020-10-06 22:42:15 +0200

    Fixed the node version handling in build scripts

--- scripts/prebuild-publish.bat
+++ scripts/prebuild-publish.bat
@@ -2,7 +2,7 @@
 
 if %APPVEYOR_REPO_BRANCH% == master (
   if %GITHUB_TOKEN% neq "" (
-    npm run prebuild-release -- -u %GITHUB_TOKEN% -t %1.0.0 -r node
-    npm run prebuild-release -- -u %GITHUB_TOKEN% -t %1.0.0 -r electron
+    npm run prebuild-release -- -u %GITHUB_TOKEN% -t %NODEJS_VERSION%.0.0 -r node
+    npm run prebuild-release -- -u %GITHUB_TOKEN% -t %NODEJS_VERSION%.0.0 -r electron
   )
 )
--- scripts/prebuild-publish.sh
+++ scripts/prebuild-publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $GITHUB_TOKEN ]]; then
-  npm run prebuild-release -- -u "$GITHUB_TOKEN" -t $TRAVIS_NODE_VERSION -r node
-  npm run prebuild-release -- -u "$GITHUB_TOKEN" -t $TRAVIS_NODE_VERSION -r electron
+  npm run prebuild-release -- -u "$GITHUB_TOKEN" -t $TRAVIS_NODE_VERSION.0.0 -r node
+  npm run prebuild-release -- -u "$GITHUB_TOKEN" -t $TRAVIS_NODE_VERSION.0.0 -r electron
 fi
